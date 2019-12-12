20204325523b39be416b37caf61acc0793905c27
fatimah <fatemeh.naserzadeh01@gmail.com>
2019-12-12 16:24:37 +0330

    Init commit

--- views.py
+++ views.py
@@ -1,70 +1,76 @@
-from flask import Flask,render_template
-from prettytable import PrettyTable
+from flask import Flask,render_template,url_for,request
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import normalize
+import  matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 app = Flask(__name__)
-
-def to_html_converter(path):
-    path = 'data/{}.csv'.format(path)
-    csv_file = open(path,'r')
-    csv_file = csv_file.readlines()
-
-    #creates a list of 6 first items of csv_file
-    line = [] 
-    for i in range(6):
-        line.append(csv_file[i])
-    #splits items at ','
-    for i in range(len(line)):
-        line[i] = line[i].split(',')
-    
-    x = PrettyTable()
-    column_names = []
-    for i in range(len(line[0])):
-        column_names.append(line[0][i])
-    # column_names = [line[0][0],line[0][1],line[0][2],line[0][3]]
-    for i in range(len(column_names)):
-        x.add_column(column_names[i],[line[1][i],line[2][i],line[3][i],line[4][i],line[5][i]])
-    html_code = x.get_html_string()
-    html_file = open('templates/table.html','w')
-    html_file = html_file.write(html_code)
-    
-
-
-
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-
-
-@app.route('/airbase_data')
-def airbase():
-    to_html_converter('airbase_data')
-    return render_template('table.html')
-
-
-
 @app.route('/titanic')
 def titanic():
-    to_html_converter('titanic')
-    return render_template('table.html')
+    return render_template('titanic.html')
 
 
 
-@app.route('/flowdata')
-def flowdata():
-    to_html_converter('flowdata')
-    return render_template('table.html')
 
 
 
-@app.route('/melb_data')
-def melb():
-    to_html_converter('melb_data')
-    return render_template('table.html')
+def preparing_data():
+    titanic = sns.load_dataset('titanic')
+    titanic = titanic.drop(['sex','embarked','class','who','adult_male','deck','embark_town','alive','alone'],axis = 1)
+    titanic['age'] = titanic['age'].fillna(method = 'ffill')
+
+    for label in ['pclass','age','sibsp','parch','fare']:
+        titanic[label] = LabelEncoder().fit_transform(titanic[label])
+    
+    labels = titanic['survived']
+    features = titanic.drop(['survived'],axis=1)
+    model = PCA(n_components=3)
+    model.fit(features)
+    X_3D = model.transform(features)
+    data = [X_3D,labels]
+    return data
 
 
 
+
+def display_data():
+    data = preparing_data()
+    labels = data[1]
+    X_3D = data[0]
+    live_x,live_y,live_z = X_3D[labels==1].T
+    dead_x,dead_y,dead_z = X_3D[labels==0].T
+
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111,projection='3d')
+
+    ax.scatter(live_x,live_y,live_z,c='r',
+    label='Casualties',marker='o')
+    ax.scatter(dead_x,dead_y,dead_z,c='b',label='Survivors',marker='^')
+    ax.legend()
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.savefig("./static/Figure_1.png")
+
+
+
+
+
+
+display_data()
 
 
 
